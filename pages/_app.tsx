9390562d2baa2ccb48347371a3d55d353437a3e5
Gabriela Harea <hareagabi@gmail.com>
2021-11-10 10:57:54 +0200

    I modified the UI material

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app';
-import { CacheProvider, ThemeProvider } from '@emotion/react';
-import createEmotionCache from './createEmotionCache';
-import { theme } from "./theme";
-import { FC } from "react";
 
+import React, { useEffect, FC } from "react";
+import { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import { EmotionCache } from "@emotion/cache";
+import createEmotionCache from './createEmotionCache';
+import { CacheProvider } from "@emotion/react";
+// Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  // eslint-disable-next-line react/require-default-props
+  emotionCache?: EmotionCache;
+}
 
 const App: FC<AppProps> = ({
   Component, 
@@ -13,8 +19,11 @@ const App: FC<AppProps> = ({
   emotionCache = clientSideEmotionCache, pageProps,}) => {
     return ( 
       <CacheProvider value = {emotionCache}>
-          <ThemeProvider theme = {theme} > </ThemeProvider>
+          <ThemeProvider theme = {theme}> 
+          </ThemeProvider>
           <Component {...pageProps} />;
       </CacheProvider>
     );
   }; 
+
+  export default App;
